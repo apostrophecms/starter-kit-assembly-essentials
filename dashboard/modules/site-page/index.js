a9f263934e31e6d231c66dfbebf8191a3c23abc2
Tom Boutell <tom@apostrophecms.com>
2021-02-16 14:59:45 -0500

    clean up the site creation experience, requires matching multisite PR

--- dashboard/modules/site-page/index.js
+++ dashboard/modules/site-page/index.js
@@ -2,13 +2,25 @@ module.exports = {
   extend: '@apostrophecms/piece-page-type',
   options: {
     label: 'Site Page',
-    perPage: 50,
-    contextual: true
+    perPage: 50
   },
   methods(self, options) {
     return {
       chooseParentPage(pages, piece) {
         return null;
+      }
+    };
+  },
+  extendMethods(self, options) {
+    return {
+      indexQuery(_super, req) {
+        const query = _super(req);
+        query.and({
+          temporary: {
+            $ne: true
+          }
+        });
+        return query;
       }
     };
   }
