9f69c8e80a4ce7b8f552fdc371275a782c220f05
Tom Boutell <tom@apostrophecms.com>
2021-02-17 17:06:50 -0500

    Grouping

--- sites/modules/@apostrophecms/global/index.js
+++ sites/modules/@apostrophecms/global/index.js
@@ -2,6 +2,12 @@ module.exports = {
   fields: {
     add: {
       ...require('@apostrophecms-pro/basics/lib/navigation')
-    }
+    },
+    group: {
+      navigation: {
+        label: 'Navigation',
+        fields: [ 'navLogo', 'navLogoAlignment', 'navLinks' ]
+      }
+    }    
   }
 };
