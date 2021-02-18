3d2b83aad4d7a33de3761f266ef025fbb9cb2c6a
Alex Bea <abea@mailbox.org>
2021-02-18 15:05:01 -0600

    Makes column widget the only top level in page main areas

--- sites/modules/default-page/index.js
+++ sites/modules/default-page/index.js
@@ -1,3 +1,5 @@
+const richText = require('@apostrophecms-pro/basics/lib/rich-text');
+
 module.exports = {
   extend: '@apostrophecms/page-type',
   fields: {
@@ -6,7 +8,22 @@ module.exports = {
         type: 'area',
         label: 'Main',
         options: {
-          widgets: require('@apostrophecms-pro/basics/lib/widgets').widgets
+          widgets: {
+            '@apostrophecms-pro/basics-column': {
+              widgets: {
+                '@apostrophecms/rich-text': {
+                  styles: richText.styles,
+                  toolbar: richText.toolbars.BASE
+                },
+                '@apostrophecms-pro/basics-image': {},
+                '@apostrophecms-pro/basics-button': {},
+                '@apostrophecms-pro/basics-card': {},
+                '@apostrophecms-pro/basics-hero': {},
+                '@apostrophecms/video': {},
+                '@apostrophecms/html': {}
+              }
+            }
+          }
         }
       }
     }
