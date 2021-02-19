672d498881756d085e0ad2b4a8a52d01e17e9782
Ben Loeffler <bb@Bens-MBP.home>
2021-02-19 13:57:54 -0500

    spelled dashboard right

--- dashboard/modules/@apostrophecms/page/index.js
+++ dashboard/modules/@apostrophecms/page/index.js
@@ -8,7 +8,7 @@ module.exports = {
         slug: '/',
         parkedId: 'home',
         _defaults: {
-          title: 'Dashbaord',
+          title: 'Dashboard',
           type: 'site-page'
         },
         _children: [
