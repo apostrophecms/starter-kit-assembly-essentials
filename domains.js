7cfac3d9c087b480296398bcc72f6c30d3d1aab1
Tom Boutell <tom@apostrophecms.com>
2021-02-25 14:36:04 -0500

    production domain for testing

--- domains.js
+++ domains.js
@@ -3,5 +3,5 @@ module.exports = {
   // Should be a real registered domain with a DNS wildcard pointing to the cloud
   staging: 'CHANGEME.dev',
   // Should be a real registered domain with a DNS wildcard pointing to the cloud
-  prod: 'multisite-boilerplate.app'
+  prod: 'a3-assembly-demo.apostrophecms.com'
 };
