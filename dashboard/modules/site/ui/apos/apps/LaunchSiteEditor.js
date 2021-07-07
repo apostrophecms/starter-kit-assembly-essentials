24bafe19e0f413e34152c1d5ed770ff5fc394767
Alex Bea <abea@mailbox.org>
2021-07-07 11:12:44 -0500

    Moves column styles to theme to use gutter var

--- dashboard/modules/site/ui/apos/apps/LaunchSiteEditor.js
+++ dashboard/modules/site/ui/apos/apps/LaunchSiteEditor.js
@@ -5,7 +5,7 @@ export default function() {
     }
     e.preventDefault();
     e.stopPropagation();
-    // TODO wrap in modules ready?
+
     apos.bus.$emit('admin-menu-click', {
       itemName: 'site:editor',
       props: {
