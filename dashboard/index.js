97f8802fa11ded7884c5e86cde1780f114d4114b
Tom Boutell <tom@apostrophecms.com>
2021-07-23 13:53:35 -0400

    see if I can clear up what's happening on the dashboard login page while I wait for the PR to go through on multisite

--- dashboard/index.js
+++ dashboard/index.js
@@ -3,6 +3,7 @@ const domains = require('../domains.js');
 module.exports = {
   stagingBaseUrl: `https://dashboard.${domains.staging}`,
   prodBaseUrl: `https://dashboard.${domains.prod}`,
+  privateDashboards: true,
   modules: {
     '@apostrophecms/attachment': {
       options: {
