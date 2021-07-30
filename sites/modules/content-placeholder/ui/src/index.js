76780c2abc32e2c20759c05be111d50258cb31ad
Stuart Romanek <stuart@apostrophecms.com>
2021-07-30 15:37:53 -0400

    $el -> el

--- sites/modules/content-placeholder/ui/src/index.js
+++ sites/modules/content-placeholder/ui/src/index.js
@@ -9,12 +9,12 @@ export default () => {
 
   function checkModeAndToggleVisibility() {
     const editMode = window.apos.modules['@apostrophecms/admin-bar'].editMode || false;
-    const $el = document.querySelector('[data-apos-placeholder-container]');
-    if ($el) {
-      if (editMode && !$el.classList.contains(hiddenClass)) {
-        apos.util.addClass($el, hiddenClass);
-      } else if (!editMode && $el.classList.contains(hiddenClass)) {
-        apos.util.removeClass($el, hiddenClass);
+    const el = document.querySelector('[data-apos-placeholder-container]');
+    if (el) {
+      if (editMode && !el.classList.contains(hiddenClass)) {
+        apos.util.addClass(el, hiddenClass);
+      } else if (!editMode && el.classList.contains(hiddenClass)) {
+        apos.util.removeClass(el, hiddenClass);
       }
     }
   }
