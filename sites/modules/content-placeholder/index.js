fe56267257f6f2aede01ac38adf9f4509fe990b7
Stuart Romanek <stuart@apostrophecms.com>
2021-07-30 10:00:40 -0400

    adds placeholder nudger, moves static help text to readme

---
+++ sites/modules/content-placeholder/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  options: {
+    // A pointer to get started
+    alias: 'pointer'
+  }
+};
---
+++ sites/modules/content-placeholder/ui/src/index.js
@@ -0,0 +1,18 @@
+export default () => {
+  const hiddenClass = 'c-placeholder--is-hidden';
+  setTimeout(() => {
+    setInterval(checkModeAndToggleVisibility, 300);
+  }, 200);
+
+  function checkModeAndToggleVisibility() {
+    const editMode = window.apos.modules['@apostrophecms/admin-bar'].editMode || false;
+    const $el = document.querySelector('[data-apos-placeholder-container]');
+    if ($el) {
+      if (editMode && !$el.classList.contains(hiddenClass)) {
+        apos.util.addClass($el, hiddenClass);
+      } else if (!editMode && $el.classList.contains(hiddenClass)) {
+        apos.util.removeClass($el, hiddenClass);
+      }
+    }
+  }
+};
