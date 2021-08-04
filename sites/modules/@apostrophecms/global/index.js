662d94bbcd10732fa1e8370408b797f720ff1f70
Stuart Romanek <stuart@apostrophecms.com>
2021-08-04 09:47:15 -0400

    use qs module, collapse some testing

--- sites/modules/@apostrophecms/global/index.js
+++ sites/modules/@apostrophecms/global/index.js
@@ -41,19 +41,20 @@ module.exports = {
         addFontFamilies(req, doc, options) {
           if (req.data.global) {
             const choices = [];
-            let query;
+            let parsedQuery = null;
             // parse tag for quoted strings (attribute values)
-            const values = doc.googleFontScript.match(/"(\\.|[^"\\])*"/g);
-            values.forEach(str => {
-              // remove quotes and return the query string portion of the url
-              const qs = str.split('"').join('').split('?');
-              if (qs.length > 1) {
-                // has a query string, make sure it has a 'family' param
-                query = queryStringToJSON(qs[1]).family ? queryStringToJSON(qs[1]) : null;
+            const quotedStrings = doc.googleFontScript.match(/"(\\.|[^"\\])*"/g);
+            quotedStrings.forEach(str => {
+              // get just querystring portion of url
+              const test = str.split('"').join('').split('?');
+
+              // is a query string and has a 'family' property
+              if (test.length > 1 && qs.parse(test[1]).family) {
+                parsedQuery = qs.parse(test[1]);
               }
             });
-            if (query) {
-              query.family.forEach(family => {
+            if (parsedQuery) {
+              parsedQuery.family.forEach(family => {
                 const fontFamily = family.split(':')[0].split('+').join(' ');
                 const variantChoices = [];
                 const variants = family.split('@')[1] ? family.split('@')[1].split(';') : [ '400' ];
@@ -70,26 +71,6 @@ module.exports = {
             }
             doc.fontFamilies = choices;
           }
-          // https://stackoverflow.com/a/32204670/7998226
-          function queryStringToJSON(qs) {
-            var pairs = qs.split('&');
-            var result = {};
-            pairs.forEach(function(p) {
-              var pair = p.split('=');
-              var key = pair[0];
-              var value = decodeURIComponent(pair[1] || '');
-              if (result[key]) {
-                if (Object.prototype.toString.call(result[key]) === '[object Array]') {
-                  result[key].push(value);
-                } else {
-                  result[key] = [ result[key], value ];
-                }
-              } else {
-                result[key] = value;
-              }
-            });
-            return JSON.parse(JSON.stringify(result));
-          };
         }
       }
     };
