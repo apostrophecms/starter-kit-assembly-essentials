7a49bc07cc37efa38ace3e30e954d4358645a62d
Thomas Boutell <tom@apostrophecms.com>
2021-11-11 09:24:07 -0500

    test drive with mongodb multiplexer. Uses a hardcoded name for the real database for now, so not ready to be a PR

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
+const mongo = require('mongodb');
+const mm = require('@apostrophecms-pro/mongodb-multiplexer');
+
 // Domain name configuration is in domains.js
 const domains = require('./domains.js');
 
 const dashboardHostnames = Object.values(domains).map(domain => `dashboard.${domain}`.replace(/:\d+$/, ''));
 
-require('@apostrophecms-pro/multisite')({
-  // Default port, for dev
-  port: 3000,
-  // Change this to a hardcoded string when forking to make a new project.
-  // Just set it to a string which should never change. Ideally should match
-  // your repo name followed by a -, however if you plan to use a
-  // cheap Atlas cluster (below M10), you must use a unique prefix less
-  // than 12 characters (before the -).
-  shortNamePrefix: process.env.APOS_PREFIX || 'a3ab-',
-  // For development. An environment variable overrides this in staging/production
-  mongodbUrl: 'mongodb://localhost:27017',
-  sessionSecret: 'CHANGEME',
-  sites: require('./sites/index.js'),
-  dashboard: require('./dashboard/index.js'),
-  dashboardHostname: dashboardHostnames
-}).then(function (result) {
+go().then(function (result) {
   // There is no top level await so we catch this here.
   // At this point either the task is running or the site is up.
   // A task will exit on its own.
@@ -26,3 +14,32 @@ require('@apostrophecms-pro/multisite')({
   console.error(err);
   process.exit(1);
 });
+
+async function go() {
+  if (process.env.MONGODB_MULTIPLEXER) {
+    const realClient = await mongo.MongoClient.connect('mongodb://localhost:27017', {
+      useUnifiedTopology: true
+    });
+    const realDb = realClient.db('mm-test');
+    client = mm({
+      db: realDb
+    });
+  }
+  await require('@apostrophecms-pro/multisite')({
+    // Default port, for dev
+    port: 3000,
+    // Change this to a hardcoded string when forking to make a new project.
+    // Just set it to a string which should never change. Ideally should match
+    // your repo name followed by a -, however if you plan to use a
+    // cheap Atlas cluster (below M10), you must use a unique prefix less
+    // than 12 characters (before the -).
+    shortNamePrefix: process.env.APOS_PREFIX || 'a3ab-',
+    // For development. An environment variable overrides this in staging/production
+    mongodbUrl: 'mongodb://localhost:27017',
+    sessionSecret: 'CHANGEME',
+    sites: require('./sites/index.js'),
+    dashboard: require('./dashboard/index.js'),
+    dashboardHostname: dashboardHostnames,
+    client
+  });
+}
