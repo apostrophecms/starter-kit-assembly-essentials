84f21fdbed09cf710d637efaa7fe6010b88d3d02
Jed <vjeudy@protonmail.com>
2022-07-06 10:06:32 +0200

    rollback to dev domain

--- domains.js
+++ domains.js
@@ -1,5 +1,5 @@
 module.exports = {
-  local: 'localhost:3000',
+  dev: 'localhost:3000',
   // Should be a real registered domain or subdomain with a DNS wildcard pointing to the cloud
   staging: 'a3-assembly-staging.apostrophecms.com',
   // Should be a real registered domain or subdomain with a DNS wildcard pointing to the cloud
