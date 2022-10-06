6224e2fcce49f3d4e61b744a75ea912154c02b7a
Thomas Boutell <tom@apostrophecms.com>
2022-10-06 11:56:12 -0400

    typo

--- sites/modules/theme-default/ui/src/index.js
+++ sites/modules/theme-default/ui/src/index.js
@@ -1,6 +1,6 @@
 export default () => {
   console.log('Default theme project level js file');
-  // Uncomment to demonstrate a websocket conne ction
+  // Uncomment to demonstrate a websocket connection
   // // Simple test that works locally and in the cloud: http -> ws, https -> wss
   // const url = window.location.href.replace(/^http/, 'ws');
   // const ws = new WebSocket(url);
