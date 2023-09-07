33b90b63fdf0c868fc28fbe57e33c3233ec8006b
Thomas Boutell <tom@apostrophecms.com>
2023-09-07 16:07:18 -0400

    sample gcs config with env vars

--- dashboard/index.js
+++ dashboard/index.js
@@ -3,7 +3,14 @@ module.exports = {
   modules: {
     '@apostrophecms/uploadfs': {
       options: {
-        uploadfs: {
+        uploadfs: process.env.GOOGLE_APPLICATION_CREDENTIALS ? {
+          storage: 'gcs',
+          // Go to the Google Cloud Console, select your project and select the Storage item on the left side
+          // of the screen to find / create your bucket. Put your bucket name here.
+          bucket: process.env.GOOGLE_BUCKET,
+          // Select your region
+          region: process.env.GOOGLE_REGION
+        }: {
           disabledFileKey: 'CHANGEME'
         }
       }
--- sites/index.js
+++ sites/index.js
@@ -8,8 +8,14 @@ module.exports = function (site) {
     modules: {
       '@apostrophecms/uploadfs': {
         options: {
-          uploadfs: {
-            // Be sure to change
+          uploadfs: process.env.GOOGLE_APPLICATION_CREDENTIALS ? {
+            storage: 'gcs',
+            // Go to the Google Cloud Console, select your project and select the Storage item on the left side
+            // of the screen to find / create your bucket. Put your bucket name here.
+            bucket: process.env.GOOGLE_BUCKET,
+            // Select your region
+            region: process.env.GOOGLE_REGION
+          }: {
             disabledFileKey: 'CHANGEME'
           }
         }
