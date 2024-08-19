2894c93067d5272baec5202f3931e87586700aa1
Robert Means <robert@robertmeans.net>
2024-08-19 13:32:54 -0400

    making recipe for GrypTech

---
+++ sites/modules/@apostrophecms-pro/basics-hero-widget/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  fields: {
+    add: {
+      randomString: {
+        type: 'string',
+        label: 'Random String',
+        help: 'A random string to demonstrate adding fields to the widget'
+      }
+    }
+  }
+};
