3ff7d9e1c8eb71f88a58b8fe97a9c6613afb51b5
Harouna Traoré <haroun@users.noreply.github.com>
2024-10-11 17:26:13 +0200

    restore port 3000

--- domains.js
+++ domains.js
@@ -1,6 +1,7 @@
 export default {
-  local: 'localhost:3002',
-  // Should be a real registered domain or subdomain with a DNS wildcard pointing to the cloud
+  local: 'localhost:3000',
+  // Should be a real registered domain or subdomain with a
+  // DNS wildcard pointing to the cloud
   staging: 'a3-assembly-staging.apostrophecms.com',
   // Should be a real registered domain or subdomain with a
   // DNS wildcard pointing to the cloud
